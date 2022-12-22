45bd91ce4ec63d6855452637ccfb5a681a33cc27
Yumeng Xu <yumeng.xu@physik.uzh.ch>
2022-12-22 11:23:48 +0100

    add inet option support

--- cwb.py
+++ cwb.py
@@ -1,4 +1,6 @@
-def cwb_inet2G(ROOT, gROOT, config, run_id, f_name, j_stage, u_name = "", eced = False):
+import os
+
+def cwb_inet2G(ROOT, gROOT, config, run_id, f_name, j_stage, u_name = "", eced = False, inet_option=None):
 	gROOT.LoadMacro(f"{config['CWB']['CWB_INSTALL']}/etc/cwb/macros/cwb_inet2G.C")
 
 	# TODO: parameters check
@@ -13,4 +15,7 @@ def cwb_inet2G(ROOT, gROOT, config, run_id, f_name, j_stage, u_name = "", eced =
 		"SAVE": 7,
 		"FINISH": 8,
 	}
+
+	if inet_option:
+		os.environ['CWB_INET_OPTIONS'] = inet_option
 	ROOT.cwb_inet2G(run_id, f_name, CWB_STAGE[j_stage], u_name, eced)
