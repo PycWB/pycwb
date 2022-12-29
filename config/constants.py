b0929714dd770470de3ebc8aaac20e89d68669a1
Yumeng Xu <yumeng.xu@physik.uzh.ch>
2022-12-29 16:06:24 +0100

    seperate constants

---
+++ config/constants.py
@@ -0,0 +1,22 @@
+CWB_CAT = {
+    "CWB_CAT0": 0,
+    "CWB_CAT1": 1,
+    "CWB_CAT2": 2,
+    "CWB_CAT3": 3,
+    "CWB_HVETO": 10,
+    "CWB_PEM": 11,
+    "CWB_EXC ": 12,
+    "CWB_USER": 13
+}
+
+CWB_STAGE = {
+    "FULL": 0,
+    "INIT": 1,
+    "STRAIN": 2,
+    "CSTRAIN": 3,
+    "COHERENCE": 4,
+    "SUPERCLUSTER": 5,
+    "LIKELIHOOD": 6,
+    "SAVE": 7,
+    "FINISH": 8,
+}
--- cwb.py
+++ cwb.py
@@ -1,22 +1,15 @@
+# cwb.py is a module to convert cwb functionality to python functions
+
 import os
+from pycWB.config.constants import CWB_STAGE
 
-def cwb_inet2G(ROOT, gROOT, config, run_id, f_name, j_stage, u_name = "", eced = False, inet_option=None):
-	gROOT.LoadMacro(f"{config['CWB']['CWB_INSTALL']}/etc/cwb/macros/cwb_inet2G.C")
 
-	# TODO: parameters check
-	CWB_STAGE = {
-		"FULL": 0,
-		"INIT": 1,
-		"STRAIN": 2,
-		"CSTRAIN": 3,
-		"COHERENCE": 4,
-		"SUPERCLUSTER": 5,
-		"LIKELIHOOD": 6,
-		"SAVE": 7,
-		"FINISH": 8,
-	}
+def cwb_inet2G(ROOT, gROOT, config, run_id, f_name, j_stage, u_name="", eced=False, inet_option=None):
+    gROOT.LoadMacro(f"{config['CWB']['CWB_INSTALL']}/etc/cwb/macros/cwb_inet2G.C")
 
-	os.environ['CWB_JOBID'] = str(run_id)
-	if inet_option:
-		os.environ['CWB_INET_OPTIONS'] = inet_option
-	ROOT.cwb_inet2G(run_id, f_name, CWB_STAGE[j_stage], u_name, eced)
+    # TODO: parameters check
+
+    os.environ['CWB_JOBID'] = str(run_id)
+    if inet_option:
+        os.environ['CWB_INET_OPTIONS'] = inet_option
+    ROOT.cwb_inet2G(run_id, f_name, CWB_STAGE[j_stage], u_name, eced)
