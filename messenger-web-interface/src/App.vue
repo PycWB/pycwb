86379dad4fb8d45588ae1847f8182d38bd403e8f
Yumeng Xu <yumeng.xu@physik.uzh.ch>
2023-02-03 09:55:08 +0100

    add workflow example

--- messenger-web-interface/src/App.vue
+++ messenger-web-interface/src/App.vue
@@ -1,85 +1,45 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import drawflow from './views/drawflow.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="cWB logo" class="logo" src="@/assets/cwb_icon_modern_blue_alfa.png" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <drawflow/>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style>
+*,
+:after,
+:before {
+  box-sizing: border-box;
+  margin: 0;
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+
+}
+html {
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+  background: transparent;
+}
+body {
+  max-width: calc(100vw - 40px);
+  max-height:  calc(100vh - 40px);
+  margin: 20px;
+  padding: 0;
+  background: #181818;
+  color: #f7f7f7;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+#app {
+  margin: 0px;
+  border-radius: 8px;
+  border: 1px solid #494949;
+
+}
+.el-main {
+  padding: 0;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- messenger-web-interface/src/main.js
+++ messenger-web-interface/src/main.js
@@ -1,13 +1,15 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as Icons from '@element-plus/icons'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
-
 const app = createApp(App)
 
+app.use(ElementPlus)
+app.use(Icons)
 app.use(createPinia())
 app.use(router)
 
