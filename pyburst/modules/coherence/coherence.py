c4e45600223c1dfa78e0047e6260cd75ad3c6a09
Yumeng Xu <yumeng.xu@physik.uzh.ch>
2023-04-07 12:43:29 +0200

    merge coherence_parallel to coherence

--- pyburst/modules/coherence/coherence.py
+++ pyburst/modules/coherence/coherence.py
@@ -8,58 +8,10 @@ from pyburst.modules.network import create_network
 from pyburst.types import TimeFrequencySeries, FragmentCluster
 from pyburst.conversions import convert_to_wavearray
 
-
 logger = logging.getLogger(__name__)
 
 
-def coherence_parallel(config, tf_maps, wdm_list, nRMS_list):
-    """
-    Calculate the coherence parallelly, a temporary network object will be created for each process to avoid conflict.
-    MRA won't be loaded for the temporary network object.
-
-    Process each resolution level in parallel
-
-    * Loop over detectors (cwb::nIFO)
-
-      * Compute the maximum energy of TF pixels (WSeries<double>::maxEnergy)
-    * Set pixel energy selection threshold (network::THRESHOLD)
-    * Loop over time lags (network::nLag)
-
-      * Select the significant pixels (network::getNetworkPixels)
-      * Single resolution clustering (network::cluster)
-
-    :param config: user configuration
-    :type config: Config
-    :param tf_maps: list of strain
-    :type tf_maps: list[TimeFrequencySeries]
-    :param wdm_list: list of wdm
-    :type wdm_list: list[WDM]
-    :param nRMS_list: list of noise RMS
-    :type nRMS_list: list[TimeFrequencySeries]
-    :return: fragment_clusters
-    :rtype: list[FragmentCluster]
-    """
-    timer_start = time.perf_counter()
-    logger.info("Start coherence")
-
-    up_n = config.rateANA // 1024
-    if up_n < 1:
-        up_n = 1
-
-    fragment_clusters = []
-
-    with Pool(processes=min(config.nproc, config.nRES)) as pool:
-        tasks = []
-        for i in range(config.nRES):
-            tasks.append((i, config, tf_maps, nRMS_list, wdm_list[i], up_n))
-        for fragment_clusters_single_res in pool.starmap(_coherence_single_res, tasks):
-            fragment_clusters += fragment_clusters_single_res
-
-    logger.info("Coherence time totally: %f s", time.perf_counter() - timer_start)
-    return fragment_clusters
-
-
-def coherence(config, tf_maps, wdm_list, nRMS_list, net=None):
+def coherence(config, tf_maps, wdm_list, nRMS_list, net=None, parallel=True):
     """
     Select the significant pixels
 
@@ -84,23 +36,34 @@ def coherence(config, tf_maps, wdm_list, nRMS_list, net=None):
     :type nRMS_list: list[TimeFrequencySeries]
     :param net: network, if None, create a temporary minimum network object with wdm_list and nRMS_list
     :type net: ROOT.network, optional
+    :param parallel: whether to use parallel
+    :type parallel: bool, optional
     :return: fragment_clusters
     :rtype: list[FragmentCluster]
     """
     # calculate upsample factor
     timer_start = time.perf_counter()
+    logger.info("Start coherence" + " in parallel" if parallel else "")
     up_n = config.rateANA // 1024
     if up_n < 1:
         up_n = 1
 
     fragment_clusters = []
 
-    for i in range(config.nRES):
-        fragment_clusters_single_res = _coherence_single_res(i, config, tf_maps, nRMS_list, wdm_list[i], up_n, net)
-        fragment_clusters += fragment_clusters_single_res
+    if parallel:
+        with Pool(processes=min(config.nproc, config.nRES)) as pool:
+            tasks = []
+            for i in range(config.nRES):
+                tasks.append((i, config, tf_maps, nRMS_list, wdm_list[i], up_n))
+            for fragment_clusters_single_res in pool.starmap(_coherence_single_res, tasks):
+                fragment_clusters += fragment_clusters_single_res
+    else:
+        for i in range(config.nRES):
+            fragment_clusters_single_res = _coherence_single_res(i, config, tf_maps, nRMS_list, wdm_list[i], up_n, net)
+            fragment_clusters += fragment_clusters_single_res
 
     logger.info("----------------------------------------")
-    logger.info("Coherence time: %f s", time.perf_counter() - timer_start)
+    logger.info("Coherence time totally: %f s", time.perf_counter() - timer_start)
     logger.info("----------------------------------------")
 
     return fragment_clusters
@@ -220,3 +183,6 @@ def _coherence_single_res(i, config, tf_maps, nRMS_list, wdm, up_n, net=None):
 
     logger.info(logger_info)
     return fragment_clusters
+
+
+coherence_parallel = coherence
