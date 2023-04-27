c70e8b7fdac91cd2a73c4512ffd571b11cd56e59
Yumeng Xu <yumeng.xu@physik.uzh.ch>
2023-04-27 17:29:57 +0200

    update examples

---
+++ examples/batch_injection/pycbc_inject/hyperbolicTD/__init__.py
@@ -0,0 +1,11 @@
+# import pycbc.waveform as waveform
+# from pycbc.waveform import parameters, get_td_waveform
+
+
+# from pycbc_inject.waveform import get_td_waveform as get_td_waveform_inj
+# import pycbc_inject.parameters as parameters_inj
+
+# waveform.parameters.td_waveform_params = parameters_inj.td_waveform_params
+# parameters.td_waveform_params = parameters_inj.td_waveform_params
+# waveform.get_td_waveform = get_td_waveform_inj
+# get_td_waveform = get_td_waveform_inj
