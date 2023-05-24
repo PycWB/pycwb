fc8fc7e4f5bbe04d08d93dd65bb1a9efc8e7cf0f
Yumeng Xu <yumeng.xu@physik.uzh.ch>
2023-05-24 15:12:13 +0200

    remove tests file in git

--- tests/pyburst_mdc_test.py
+++
@@ -1,8 +0,0 @@
-# load user parameters
-import sys
-# add the parent directory to the first position of the path
-sys.path.insert(0, '..')
-from pyburst.search import search
-
-
-search('./user_parameters_mdc.yaml', no_subprocess=True)
