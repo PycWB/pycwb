64610ad054859c891cb93f72266be26afc548649
Yumeng Xu <yumeng.xu@physik.uzh.ch>
2023-06-01 11:40:43 +0200

    show the path of ROOT

--- pycwb/utils/check_ROOT.py
+++ pycwb/utils/check_ROOT.py
@@ -12,17 +12,17 @@ def check_and_load_wavelet(ROOT):
     :return:
     """
     if not hasattr(ROOT, "WDM"):
-        logger.info("Loading wavelet library")
+        print("Loading wavelet library")
     try:
         site_packages = next(p for p in sys.path if 'site-packages' in p)
         wavelet_path = f"{site_packages}/lib/wavelet"
-        logger.info(f"Trying to load wavelet library from {wavelet_path}")
+        print(f"Trying to load wavelet library from {wavelet_path}")
         ROOT.gInterpreter.AddIncludePath(f"{site_packages}/include")
         ROOT.gSystem.Load(wavelet_path)
     except:
-        logger.error("Cannot find wavelet library in PyBurst, trying to load from system")
+        print("Cannot find wavelet library in PyBurst, trying to load from system")
         try:
             ROOT.gSystem.Load("wavelet")
         except:
-            logger.error("Cannot find wavelet library")
+            print("Cannot find wavelet library")
             raise Exception("Cannot find wavelet library")
