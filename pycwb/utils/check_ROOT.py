318065e9752221816ec6dd0d9d5439ecbe04e922
Yumeng Xu <yumeng.xu@physik.uzh.ch>
2023-11-24 11:08:48 +0100

    fix the extension search path for debian/ubuntu system

--- pycwb/utils/check_ROOT.py
+++ pycwb/utils/check_ROOT.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import site
 
 logger = logging.getLogger(__name__)
@@ -16,6 +17,15 @@ def check_and_load_wavelet(ROOT):
     try:
         site_packages = site.getsitepackages()[0]
         wavelet_path = f"{site_packages}/lib/wavelet"
+        # if wavelet_path does not exist, try the additional path
+        if not os.path.exists(wavelet_path):
+            # if site_packages ends with dist-packages, replace it with site-packages for a second try
+            if site_packages.endswith("dist-packages"):
+                wavelet_path_additional = wavelet_path.replace("dist-packages", "site-packages")
+                if not os.path.exists(wavelet_path_additional):
+                    raise Exception(f"Cannot find wavelet library in {wavelet_path} or {wavelet_path_additional}")
+                else:
+                    wavelet_path = wavelet_path_additional
         print(f"Trying to load wavelet library from {wavelet_path}")
         ROOT.gInterpreter.AddIncludePath(f"{site_packages}/include")
         ROOT.gSystem.Load(wavelet_path)
