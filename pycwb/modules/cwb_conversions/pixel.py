e2b7f0ce519f6aa2968557d0ed508ff5428e9078
Yumeng Xu <yumeng.xu@physik.uzh.ch>
2023-12-18 14:14:35 +0100

    fix the conversion for tdAmp

--- pycwb/modules/cwb_conversions/pixel.py
+++ pycwb/modules/cwb_conversions/pixel.py
@@ -1,6 +1,8 @@
 import ROOT
-from pycwb.types.network_pixel import Pixel, PixelData
+import numpy as np
 
+from pycwb.types.network_pixel import Pixel, PixelData
+from .series import convert_to_wavearray, convert_wavearray_to_timeseries
 def convert_pixel_to_netpixel(pixel, c_id):
     """
     Convert pixel to netpixel
@@ -24,7 +26,7 @@ def convert_pixel_to_netpixel(pixel, c_id):
     netpixel.polarisation = pixel.polarisation
     netpixel.core = pixel.core
     netpixel.data = [convert_to_pixdata(d) for d in pixel.data]
-    netpixel.tdAmp = pixel.td_amp
+    netpixel.tdAmp = convert_td_amp_to_cwb(pixel.td_amp)
     netpixel.neighbors = pixel.neighbors
     return netpixel
 
@@ -46,7 +48,22 @@ def convert_netpixel_to_pixel(netpixel):
                     likelihood=netpixel.likelihood, null=netpixel.null, theta=netpixel.theta, phi=netpixel.phi,
                     ellipticity=netpixel.ellipticity, polarisation=netpixel.polarisation, core=netpixel.core,
                     data=[PixelData(d.noiserms, d.wave, d.w_90, d.asnr, d.a_90, d.rank, d.index) for d in netpixel.data],
-                    td_amp=[t for t in netpixel.tdAmp], neighbors=list(netpixel.neighbors))
+                    td_amp=np.array(netpixel.tdAmp), neighbors=list(netpixel.neighbors))
 
     return pixel
+
+
+def convert_td_amp_to_cwb(td_amps):
+    import ctypes
+    c_double_p = ctypes.POINTER(ctypes.c_double)
+
+    res = []
+    for i, data in enumerate(td_amps):
+        h = ROOT.wavearray(np.double)(len(data))
+        ROOT.pycwb_copy_to_wavearray(data.ctypes.data_as(c_double_p), h, len(data))
+        res.append(h)
+
+    return res
+
+
 
