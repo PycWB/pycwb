df523bbe9e1ad0c588ff1d03d7883a76a3e3d84b
Yumeng Xu <yumeng.xu@physik.uzh.ch>
2024-03-05 10:45:29 +0100

    set matplotlib end to avoid error running in prefect

--- pycwb/__init__.py
+++ pycwb/__init__.py
@@ -2,6 +2,8 @@ from ._version import version, __version__, version_tuple, __version_tuple__
 
 import ROOT
 from pycwb.utils.check_ROOT import check_and_load_wavelet
+import matplotlib as mpl
+mpl.use('Agg')
 
 # load wavelet library if not loaded
 check_and_load_wavelet(ROOT)
