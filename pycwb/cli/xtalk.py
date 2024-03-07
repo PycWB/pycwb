07e639f5317bc4fb168e0ea2349094966d578565
Yumeng Xu <yumeng.xu@physik.uzh.ch>
2024-03-07 11:48:02 +0100

    add cli to convert xtalk from .bin to .npz

---
+++ pycwb/cli/xtalk.py
@@ -0,0 +1,35 @@
+import os
+
+from pycwb.modules.xtalk.monster import load_catalog
+
+
+def init_parser(parser):
+    # Add the arguments
+    parser.add_argument('xtalk_file',
+                        metavar='file_path',
+                        type=str,
+                        help='the path to the user parameter file')
+
+    parser.add_argument('--output_dir',
+                        '-o',
+                        metavar='output_dir',
+                        type=str,
+                        help='the output directory')
+
+
+def command(args):
+    # Create the output directory if it does not exist
+    if not os.path.exists(args.output_dir):
+        print(f"Creating output directory: {args.output_dir}")
+        os.makedirs(args.output_dir)
+    else:
+        print(f"Output directory already exists: {args.output_dir}")
+
+    # change to the output directory
+    print(f"Changing to output directory: {args.output_dir}")
+    os.chdir(args.output_dir)
+
+    # Load the xtalk file and dump it to current directory
+    print(f"Loading xtalk file: {args.xtalk_file}")
+    load_catalog(args.xtalk_file, dump=True)
+    print(f"Xtalk file loaded and dumped to current directory: {args.output_dir}")
