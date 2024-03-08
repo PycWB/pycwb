be2da0ae3060ff3a627b72584c2f36305b7fc85f
Yumeng Xu <yumeng.xu@physik.uzh.ch>
2024-03-08 15:10:53 +0100

    remove logger in task

--- pycwb/flow/tasks/builtin.py
+++ pycwb/flow/tasks/builtin.py
@@ -1,4 +1,4 @@
-from prefect import task, get_run_logger
+from prefect import task
 import shutil
 import os
 from prefect.utilities.annotations import quote
@@ -37,33 +37,29 @@ def job_generator(nifo, slag_min, slag_max, slag_off, slag_size):
 
 @task
 def create_working_directory(working_dir):
-    logger = get_run_logger()
-
     working_dir = os.path.abspath(working_dir)
     if not os.path.exists(working_dir):
-        logger.info(f"Creating working directory: {working_dir}")
+        print(f"Creating working directory: {working_dir}")
         os.makedirs(working_dir)
     os.chdir(working_dir)
 
 
 @task
 def check_if_output_exists(working_dir, output_dir, overwrite=False):
-    logger = get_run_logger()
     output_dir = f"{working_dir}/{output_dir}"
     if os.path.exists(output_dir) and os.listdir(output_dir):
         if overwrite:
-            logger.info(f"Overwrite output directory {output_dir}")
+            print(f"Overwrite output directory {output_dir}")
         else:
-            logger.info(f"Output directory {output_dir} is not empty")
+            print(f"Output directory {output_dir} is not empty")
             raise ValueError(f"Output directory {output_dir} is not empty")
 
 
 @task
 def create_output_directory(working_dir, output_dir, log_dir, user_parameter_file):
-    logger = get_run_logger()
     # create folder for output and log
-    logger.info(f"Output folder: {working_dir}/{output_dir}")
-    logger.info(f"Log folder: {working_dir}/{log_dir}")
+    print(f"Output folder: {working_dir}/{output_dir}")
+    print(f"Log folder: {working_dir}/{log_dir}")
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     if not os.path.exists(log_dir):
@@ -83,54 +79,49 @@ def create_job_segment(config):
 
 @task
 def create_catalog_file(working_dir, config, job_segments):
-    logger = get_run_logger()
-    logger.info("Creating catalog file")
+    print("Creating catalog file")
     create_catalog(f"{working_dir}/{config.outputDir}/catalog.json", config, job_segments)
 
 
 @task
 def load_xtalk_catalog(MRAcatalog):
-    logger = get_run_logger()
-    logger.info("Loading cross-talk catalog")
+    print("Loading cross-talk catalog")
     xtalk_coeff, xtalk_lookup_table, layers, nRes = load_catalog(MRAcatalog)
-    logger.info("Cross-talk catalog loaded")
+    print("Cross-talk catalog loaded")
     return xtalk_coeff, xtalk_lookup_table, layers, nRes
 
 
 @task
 def create_web_dir(working_dir, output_dir):
-    logger = get_run_logger()
-    logger.info("Creating web directory")
+    print("Creating web directory")
     create_web_viewer(f"{working_dir}/{output_dir}")
 
 
 @task
 def check_env():
-    logger = get_run_logger()
     if not os.environ.get('HOME_WAT_FILTERS'):
-        logger.error("HOME_WAT_FILTERS is not set.")
-        logger.info("Please download the latest version of cwb config "
+        print("HOME_WAT_FILTERS is not set.")
+        print("Please download the latest version of cwb config "
                     "and set HOME_WAT_FILTERS to the path of folder XTALKS.")
-        logger.info("Make sure you have installed git lfs before cloning the repository.")
-        logger.info("For example:")
-        logger.info("    git lfs install")
-        logger.info("    git clone https://gitlab.com/gwburst/public/config_o3")
-        logger.info("    export HOME_WAT_FILTERS=$(pwd)/config_o3/XTALKS")
+        print("Make sure you have installed git lfs before cloning the repository.")
+        print("For example:")
+        print("    git lfs install")
+        print("    git clone https://gitlab.com/gwburst/public/config_o3")
+        print("    export HOME_WAT_FILTERS=$(pwd)/config_o3/XTALKS")
         raise ValueError("HOME_WAT_FILTERS is not set.")
     return True
 
 
 @task
 def print_job_info(job_seg):
-    logger = get_run_logger()
     job_id = job_seg.index
-    logger.info(f"Job ID: {job_id}")
-    logger.info(f"Start time: {job_seg.start_time}")
-    logger.info(f"End time: {job_seg.end_time}")
-    logger.info(f"Duration: {job_seg.end_time - job_seg.start_time}")
-    logger.info(f"Frames: {job_seg.frames}")
-    logger.info(f"Noise: {job_seg.noise}")
-    logger.info(f"Injections: {job_seg.injections}")
+    print(f"Job ID: {job_id}")
+    print(f"Start time: {job_seg.start_time}")
+    print(f"End time: {job_seg.end_time}")
+    print(f"Duration: {job_seg.end_time - job_seg.start_time}")
+    print(f"Frames: {job_seg.frames}")
+    print(f"Noise: {job_seg.noise}")
+    print(f"Injections: {job_seg.injections}")
 
 
 @task
