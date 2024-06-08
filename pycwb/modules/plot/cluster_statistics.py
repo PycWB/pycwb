4d9ee50e65981504822e337254ea95559c4fc8aa
Yumeng Xu <yumeng.xu@physik.uzh.ch>
2024-06-08 18:49:04 +0200

    fix filename and time tick issues

--- pycwb/modules/plot/cluster_statistics.py
+++ pycwb/modules/plot/cluster_statistics.py
@@ -5,13 +5,15 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def plot_statistics(cluster, key='likelihood', filename=None):
+def plot_statistics(cluster, key='likelihood', gps_shift=0, filename=None):
     """Plot the statistics of the event
 
     :param cluster: cluster
     :type cluster: Cluster
     :param key: key of the statistics, defaults to 'likelihood'
     :type key: str, optional
+    :param gps_shift: gps time shift, defaults to 0
+    :type gps_shift: int, optional
     :param filename: path to save the plot, defaults to None
     :type filename: str, optional
     """
@@ -20,7 +22,7 @@ def plot_statistics(cluster, key='likelihood', filename=None):
 
     merged_map, start, dt, df = cluster.get_sparse_map(key)
 
-    plt = Spectrogram(merged_map, t0=start, dt=dt, f0=0, df=df).plot()
+    plt = Spectrogram(merged_map, t0=start + gps_shift, dt=dt, f0=0, df=df).plot()
     plt.colorbar()
     # save to png
     if filename is not None:
