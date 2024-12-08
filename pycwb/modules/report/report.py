5cc87f33343967ce8c0ef4d505b3b3527713915f
Yumeng Xu <yumeng.xu@ligo.org>
2024-12-08 23:25:30 +0100

    add sigma belt

--- pycwb/modules/report/report.py
+++ pycwb/modules/report/report.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import poisson
 
 
 def report(far_rho_source=None, **kwargs):
@@ -84,13 +85,27 @@ def report_zero_lag(source, livetime_key='livetime_zerolag', far_rho_source='far
     n_events_at_ifar_min = livetime_in_years / ifar_min
     ifar_max = max(ifar)
     n_events_at_ifar_max = livetime_in_years / ifar_max
-    plt.plot([ifar_min, ifar_max], [n_events_at_ifar_min, n_events_at_ifar_max], color='black')
+    plt.plot([ifar_min, ifar_max], [n_events_at_ifar_min, n_events_at_ifar_max], color='black', linewidth=0.5)
     print(f"ifar_min: {ifar_min}, n_events_at_ifar_min: {n_events_at_ifar_min}")
     print(f"ifar_max: {ifar_max}, n_events_at_ifar_max: {n_events_at_ifar_max}")
+
+    # Calculate the Poisson confidence intervals
+    # generate the ifar range which is denser at the higher ifar
+    ifar_range = np.linspace(ifar_min, ifar_max, 100000)
+    n_events_range = livetime_in_years / ifar_range
+    sigma_levels = [1, 2, 3]
+    confidence_levels = [0.6827, 0.9545, 0.9973]
+    conf_intervals = {sigma: poisson.interval(confidence, n_events_range) for sigma, confidence in zip(sigma_levels, confidence_levels)}
+    # Plot the Poisson confidence intervals
+    colors = ['gray', 'gray', 'gray']
+    for sigma, color in zip(sigma_levels, colors):
+        lower, upper = conf_intervals[sigma]
+        plt.fill_between(ifar_range, lower, upper, color=color, alpha=0.6 / sigma, label=f'{sigma} sigma', linewidth=0.4)
 
     plt.xlabel('ifar')
     plt.ylabel('n_events')
     plt.xscale('log')
     plt.yscale('log')
+    plt.legend()
     plt.savefig(f"{source}_far_n_events.png")
     plt.close()
