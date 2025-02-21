06a05962d20fefe63a8bc7ea1b28fb0b04bb92d3
Yumeng Xu <xusmailbox@gmail.com>
2025-02-22 00:08:02 +0100

    Update benchmark file

--- benchmark/likelihood/generate_data_for_likelihood.py
+++ benchmark/likelihood/generate_data_for_likelihood.py
@@ -3,9 +3,9 @@ import os
 from pycwb.config import Config
 from pycwb.modules.logger import logger_init
 
-if not os.environ.get('HOME_WAT_FILTERS'):
-    print('Please set HOME_WAT_FILTERS to the directory of WAT filters')
-    exit(1)
+# if not os.environ.get('HOME_WAT_FILTERS'):
+#     print('Please set HOME_WAT_FILTERS to the directory of WAT filters')
+#     exit(1)
 
 logger_init()
 
@@ -14,13 +14,21 @@ config = Config('./user_parameters_injection.yaml')
 #%% md
 ## generate injected data for each detector with given parameters in config
 #%%
-from pycwb.modules.read_data import generate_injection
-from pycwb.modules.job_segment import create_job_segment_from_injection
+from pycwb.modules.read_data import generate_injection, read_from_job_segment, generate_noise_for_job_seg
+from pycwb.modules.job_segment import create_job_segment_from_injection, create_job_segment_from_config
 
-job_segments = create_job_segment_from_injection(config.ifo, config.simulation, config.injection)
+job_segments = create_job_segment_from_config(config) #create_job_segment_from_injection(config.ifo, config.simulation, config.injection)
 
-data = generate_injection(config, job_segments[0])
+job_seg = job_segments[0]
 
+data = None
+# data = generate_injection(config, job_segments[0])
+if job_seg.frames:
+    data = read_from_job_segment(config, job_seg)
+if job_seg.noise:
+    data = generate_noise_for_job_seg(job_seg, config.inRate, data=data)
+if job_seg.injections:
+    data = generate_injection(config, job_seg, data)
 #%% md
 ## apply data conditioning to the data
 #%%
