c6ab0020bcedc7feb2e948965fa848b7a634b9b0
Jiyoon Sun <jiyoon.sun@ligo.org>
2026-01-29 06:20:43 -0800

    add whitening for mdc timeseries

--- pycwb/modules/data_conditioning/__init__.py
+++ pycwb/modules/data_conditioning/__init__.py
@@ -1,4 +1,4 @@
 from .regression import *
 from .whitening_mesa import whitening_mesa
-from .whitening_cwb import whitening_cwb
+from .whitening_cwb import whitening_cwb, whitening_mdc
 from .data_conditioning import *
--- pycwb/modules/data_conditioning/whitening_cwb.py
+++ pycwb/modules/data_conditioning/whitening_cwb.py
@@ -1,7 +1,7 @@
 import numpy as np
 import ROOT
 import logging
-from pycwb.modules.cwb_conversions import convert_to_wavearray, convert_wseries_to_time_frequency_series
+from pycwb.modules.cwb_conversions import convert_to_wavearray, convert_wseries_to_time_frequency_series, convert_to_wseries
 from pycwb.types.time_frequency_series import TimeFrequencySeries
 from pycwb.types.wdm import WDM
 
@@ -14,12 +14,10 @@ def whitening_cwb(config, h):
 
     :param config: config object
     :type config: Config
-    :param wdm_white: WDM used for whitening
-    :type wdm_white: WDM
     :param h: strain data
     :type h: pycbc.types.timeseries.TimeSeries or gwpy.timeseries.TimeSeries or ROOT.wavearray(np.double)
     :return: (whitened strain, nRMS)
-    :rtype: tuple[TimeFrequencySeries, TimeFrequencySeries]
+    :rtype: tuple[pycwb.types.time_frequency_series.TimeFrequencySeries, pycwb.types.time_frequency_series.TimeFrequencySeries]
     """
     layers_white = 2 ** config.l_white if config.l_white > 0 else 2 ** config.l_high
     wdm_white = WDM(layers_white, layers_white, config.WDM_beta_order, config.WDM_precision)
@@ -69,3 +67,60 @@ def whitening_cwb(config, h):
     ##########################################
 
     return tf_map_whitened, n_rms
+
+def whitening_mdc(config, h, nRMS):
+    """
+    Performs whitening on the given strain data with provided nRMS
+
+    :param config: config object
+    :type config: Config
+    :param h: strain data
+    :type h: pycbc.types.timeseries.TimeSeries or gwpy.timeseries.TimeSeries or ROOT.wavearray(np.double)
+    :param nRMS: noise rms data
+    :type nRMS: pycwb.types.time_frequency_series.TimeFrequencySeries or ROOT.WSeries<double>
+    :return: (whitened strain, original strain)
+    :rtype: tuple[pycwb.types.time_frequency_series.TimeFrequencySeries, pycwb.types.time_frequency_series.TimeFrequencySeries]
+    """
+    layers_white = 2 ** config.l_white if config.l_white > 0 else 2 ** config.l_high
+    wdm_white = WDM(layers_white, layers_white, config.WDM_beta_order, config.WDM_precision)
+
+    # TODO: check the length of data and white parameters to prevent freezing
+    # check if whitening WDM filter lenght is less than cwb scratch
+    wdmFlen = wdm_white.m_H / config.rateANA
+    if wdmFlen > config.segEdge + 0.001:
+        logger.error("Error - filter scratch must be <= cwb scratch!!!")
+        logger.error(f"filter length : {wdmFlen} sec")
+        logger.error(f"cwb   scratch : {config.segEdge} sec")
+        raise ValueError("Filter scratch must be <= cwb scratch!!!")
+    else:
+        logger.info(f"WDM filter max length = {wdmFlen} (sec)")
+    
+    tf_map = ROOT.WSeries(np.double)(convert_to_wavearray(h), wdm_white.wavelet)
+    tf_map.Forward()
+    tf_map.setlow(config.fLow)
+    tf_map.sethigh(config.fHigh)
+
+    if isinstance(nRMS, TimeFrequencySeries):
+        nRMS = convert_to_wseries(nRMS)
+            
+    # save original data
+    hot = ROOT.WSeries(np.double)(tf_map)
+    hot.Inverse()
+
+    # whiten  0 phase WSeries
+    tf_map.white(nRMS, 1)
+    # whiten 90 phase WSeries
+    tf_map.white(nRMS, -1)
+
+    wtmp = ROOT.WSeries(np.double)(tf_map)
+    # average 00 and 90 phase
+    tf_map.Inverse()
+    wtmp.Inverse(-2)
+    tf_map += wtmp
+    tf_map *= 0.5
+
+    tf_map_whitened = convert_wseries_to_time_frequency_series(tf_map)
+    HoT = convert_wseries_to_time_frequency_series(hot)
+    wtmp.resize(0)
+
+    return tf_map_whitened, HoT
