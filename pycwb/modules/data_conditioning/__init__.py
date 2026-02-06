df3b6096178cbc46bde7ba72dfa9ca1613640b42
Alessandro Martini <alessandro.martini@ldas-pcdev12.ligo.caltech.edu>
2026-02-06 01:26:37 -0800

    whitening mdc function moved to a different module for compatibility with whitening MESA

--- pycwb/modules/data_conditioning/__init__.py
+++ pycwb/modules/data_conditioning/__init__.py
@@ -1,4 +1,5 @@
 from .regression import *
 from .whitening_mesa import whitening_mesa
-from .whitening_cwb import whitening_cwb, whitening_mdc
+from .whitening_cwb import whitening_cwb
+from .whitening_mdc import whitening_mdc
 from .data_conditioning import *
