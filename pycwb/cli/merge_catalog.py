94e8ed1e3eb07eadc789ae13e48d6b4b4f1af3a7
Jiyoon Sun <jiyoon.sun@ligo.org>
2026-02-15 09:01:12 -0800

    add function to merge wave files

--- pycwb/cli/merge_catalog.py
+++ pycwb/cli/merge_catalog.py
@@ -1,6 +1,5 @@
 import os
 
-
 def init_parser(parser):
     # working dir
     parser.add_argument('--work-dir',
@@ -10,9 +9,17 @@ def init_parser(parser):
                         default='.',
                         help='the working directory')
 
+    parser.add_argument('--wave',
+                        '-w',
+                        action='store_true',
+                        default=False,
+                        help='whether to merge the reconstructed waveforms')
 
 def command(args):
-    from pycwb.workflow.merge import merge_catalog
-
-    # Run the search function with the specified user parameter file
-    merge_catalog(working_dir=args.work_dir)
+    from pycwb.workflow.merge import merge_catalog, merge_wave
+    if not args.wave:
+        # Default: Run the search function with the specified user parameter file
+        merge_catalog(working_dir=args.work_dir)
+    else:
+        merge_wave(working_dir=args.work_dir)
+    
