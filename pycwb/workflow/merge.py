506d9371bde1f18ed0d1fd64af7aa097b95f20ad
Jiyoon Sun <jiyoon.sun@ligo.org>
2026-03-23 06:08:29 -0700

    fix file format from json to parquet

--- pycwb/workflow/merge.py
+++ pycwb/workflow/merge.py
@@ -1,55 +1,126 @@
+"""Merge catalog and waveform files from batch runs into single output files."""
+
+import os
 import glob
+import logging
 import h5py as h5
-import orjson
+from dacite import from_dict
+import pyarrow as pa
+import pyarrow.parquet as pq
+from pycwb.config import Config
+from pycwb.types.job import WaveSegment
+from pycwb.modules.catalog import Catalog
 
-def merge_catalog(working_dir: str = '.'):
-    # config_file = f"{working_dir}/config/user_parameters.yaml"
-    # # check if the config file exists
-    # if not os.path.exists(config_file):
-    #     print(f"Config file {config_file} not found")
-    #     return
-    #
-    # config = Config(config_file)
-    # catalog_dir = f"{working_dir}/{config.catalog_dir}"
+logger = logging.getLogger(__name__)
 
-    # get the list of json files catalog*.json
-    catalog_files = glob.glob(f"{working_dir}/catalog/catalog_*.json")
+def merge_catalog(working_dir: str = '.', catalog_dir: str = 'catalog', merge_label: str = None):
+    """
+    Merge the catalog files in the catalog directory from batch runs into a single catalog file.
+    
+    Parameters
+    ----------
+    working_dir : str
+        The working directory.
+    catalog_dir : str
+        The directory containing the catalog files to be merged.
+    merge_label : str, optional
+        The label to be added to the merged catalog file name. If None, the merged catalog file will be named as "catalog.parquet".
+    """
+    # get the list of parquet files catalog*.parquet
+    catalog_files = glob.glob(f"{working_dir}/{catalog_dir}/catalog_*{Catalog.DEFAULT_EXTENSION}")
     if len(catalog_files) == 0:
-        print("No catalog files found")
+        logger.warning("No catalog files found")
         return
+    
+    # merged catalog file
+    default_catalog_file = os.path.abspath(f"{working_dir}/catalog/{Catalog.DEFAULT_FILENAME}")
+    if merge_label is not None:
+        merged_catalog_file = default_catalog_file.replace(Catalog.DEFAULT_EXTENSION, f".{merge_label}{Catalog.DEFAULT_EXTENSION}")
 
-    # read the merged file catalog.json
-    with open(f"{working_dir}/catalog/catalog.json", 'r') as f:
-        merged_catalog = orjson.loads(f.read())
+        # check if the catalog file with the specified merge label already exists
+        if os.path.exists(merged_catalog_file):
+            logger.warning(f"Merged catalog file {merged_catalog_file} already exists.")
+            return
+        
+        # metadata - config
+        config = Config()
+        config.load_from_dict(Catalog.open(default_catalog_file).config)
 
-    merged_catalog["events"] = []
+        # metadata - jobs
+        jobs = []
+        for catalog_file in catalog_files:
+            catalog_file = os.path.abspath(catalog_file)
+            catalog = Catalog.open(catalog_file)
+            
+            for job in catalog.jobs:
+                jobs.append(from_dict(WaveSegment, job))
+               
+        # create the merged catalog file with the merged jobs
+        Catalog.create(merged_catalog_file, config, jobs)
+        del config, jobs
 
-    # read the sub catalog files
+    else:
+        merged_catalog_file = default_catalog_file
+    
+    # concatenate tables from all catalog files
+    tables = []
     for catalog_file in catalog_files:
-        with open(catalog_file, 'r') as f:
-            catalog = orjson.loads(f.read())
-            # append the events
-            print(f"Adding {len(catalog['events'])} events from {catalog_file}")
-            merged_catalog["events"].extend(catalog["events"])
+        catalog_file = os.path.abspath(catalog_file)
+        table = Catalog.open(catalog_file).triggers()
+        tables.append(table)
 
-    print(f"Total number of events: {len(merged_catalog['events'])}")
+        logger.info(f"Merging {catalog_file} - {len(table)} events")
+    
+    merged_table = pa.concat_tables(tables, promote_options="default")
+    logger.info(f"Total number of events: {len(merged_table)}")
 
     # write the merged catalog file
-    with open(f"{working_dir}/catalog/catalog.json", 'w') as f:
-        f.write(orjson.dumps(merged_catalog, option=orjson.OPT_INDENT_2).decode())
-    
-def merge_wave(working_dir: str = '.'):
+    logger.info(f"Save {merged_catalog_file}")
+    meta = pq.read_schema(merged_catalog_file).metadata
+    merged_table = merged_table.replace_schema_metadata(meta)
+    pq.write_table(merged_table, merged_catalog_file, compression="snappy")
+
+def merge_wave(working_dir: str = '.', output_dir: str = 'output', merge_label: str = None):
+    """
+    Merge the reconstructed waveforms in the output directory from batch runs into a single wave file.
+
+    Parameters
+    ----------
+    working_dir : str
+        The working directory.
+    output_dir : str
+        The directory containing the wave files to be merged. 
+    merge_label : str, optional
+        The label to be added to the merged wave file name. If None, the merged wave file will be named as "wave.h5".
+    """
+    default_wave_file = os.path.abspath(f"{working_dir}/output/wave.h5")
+    if merge_label is not None:
+        merged_wave_file = default_wave_file.replace('.h5', f'.{merge_label}.h5')
+
+        # check if the wave file with the specified merge label already exists
+        if os.path.exists(merged_wave_file):
+            logger.warning(f"Merged wave file {merged_wave_file} already exists.")
+            return
+    else:
+        merged_wave_file = default_wave_file
+
+    # check if the merged wave file already exists
+    if os.path.exists(merged_wave_file):
+        logger.warning(f"Merged wave file {merged_wave_file} already exists.")
+        return
+
     # get the list of hdf5 files wave_*.h5
-    wave_files = glob.glob(f"{working_dir}/output/wave_*.h5")
+    wave_files = glob.glob(f"{working_dir}/{output_dir}/wave_*.h5")
     if len(wave_files) == 0:
-        print("No waveform files found")
+        logger.warning("No waveform files found")
         return
 
     # read the merged file wave.h5
-    with h5.File(f"{working_dir}/output/wave.h5", 'w') as f:
+    logger.info(f"Create merged wave file {merged_wave_file}")
+    with h5.File(merged_wave_file, 'w') as f:
         # read the sub wave files
         for wave_file in wave_files:
             with h5.File(wave_file, 'r') as f_sub:
-                print(f"Adding waveforms of {len(f_sub.keys())} events from {wave_file}")
+                logger.info(f"Adding waveforms of {len(f_sub.keys())} events from {wave_file}")
                 for event_id in f_sub.keys():
                     f_sub.copy(event_id, f)
